e979f0bcdc4844026375b9a0185346d15096851a
Oliver Spryn <2717340+oliverspryn@users.noreply.github.com>
2019-03-08 11:38:42 -0500

    Fixes #42 Disable Crashlytics for Non-Release Builds

--- app/src/main/java/com/branhamplayer/android/ui/StartupActivity.kt
+++ app/src/main/java/com/branhamplayer/android/ui/StartupActivity.kt
@@ -4,12 +4,22 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
+import com.branhamplayer.android.BuildConfig
 import com.branhamplayer.android.R
 import com.branhamplayer.android.actions.RoutingAction
 import com.branhamplayer.android.di.DaggerInjector
 import com.branhamplayer.android.store.startupStore
+import com.google.firebase.analytics.FirebaseAnalytics
+import javax.inject.Inject
 
 class StartupActivity : AppCompatActivity() {
+
+    // region DI
+
+    @Inject
+    lateinit var firebaseAnalytics: FirebaseAnalytics
+
+    // endregion
 
     // region AppCompatActivity
 
@@ -17,7 +27,9 @@ class StartupActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.startup_activity)
 
-        DaggerInjector.buildStartupComponent(this)
+        DaggerInjector.buildStartupComponent(this).inject(this)
+
+        firebaseAnalytics.setAnalyticsCollectionEnabled(BuildConfig.BUILD_TYPE.toLowerCase() == "release")
         startupStore.dispatch(RoutingAction.NavigateToPreflightChecklistAction)
     }
 
