9e4a6ab7364622d1339840d0b02372d027d0ca05
Oliver Spryn <2717340+oliverspryn@users.noreply.github.com>
2019-03-11 16:27:13 -0400

    View's looking nice

--- sermons/src/main/java/com/branhamplayer/android/sermons/ui/PlayerFragment.kt
+++ sermons/src/main/java/com/branhamplayer/android/sermons/ui/PlayerFragment.kt
@@ -1,15 +1,22 @@
 package com.branhamplayer.android.sermons.ui
 
+import android.graphics.drawable.TransitionDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.appcompat.widget.AppCompatTextView
 import androidx.fragment.app.Fragment
+import butterknife.BindView
 import butterknife.ButterKnife
 import butterknife.Unbinder
+import com.branhamplayer.android.R as RBase
 import com.branhamplayer.android.sermons.R
 import com.branhamplayer.android.sermons.models.SermonModel
 import com.branhamplayer.android.sermons.store.sermonsStore
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import org.rekotlin.StoreSubscriber
 
 class PlayerFragment : Fragment(), StoreSubscriber<SermonModel?> {
@@ -18,7 +25,17 @@ class PlayerFragment : Fragment(), StoreSubscriber<SermonModel?> {
 
     // region UI
 
+    @BindView(R.id.player_artwork)
+    lateinit var artwork: AppCompatImageView
 
+    @BindView(R.id.player_background)
+    lateinit var background: AppCompatImageView
+
+    @BindView(R.id.player_date)
+    lateinit var date: AppCompatTextView
+
+    @BindView(R.id.player_title)
+    lateinit var title: AppCompatTextView
 
     // endregion
 
@@ -50,7 +67,25 @@ class PlayerFragment : Fragment(), StoreSubscriber<SermonModel?> {
 
     override fun newState(state: SermonModel?) {
         state?.let {
+            date.text = it.formattedDate
+            title.text = it.name
 
+            // TODO, replace with real album artwork
+            Glide.with(this)
+                .load("https://cloudinary-a.akamaihd.net/branham-player/image/upload/c_scale,w_421/samples/landscapes/beach-boat.jpg")
+                .timeout(5000)
+                .transition(DrawableTransitionOptions().crossFade())
+                .error(RBase.drawable.ic_account)
+                .placeholder(RBase.drawable.ic_sermons)
+                .centerCrop()
+                .into(artwork)
+
+            Glide.with(this)
+                .load("https://cloudinary-a.akamaihd.net/branham-player/image/upload/co_rgb:3086D4,e_colorize:80/e_blur:1600/samples/landscapes/beach-boat.jpg")
+                .timeout(5000)
+                .transition(DrawableTransitionOptions().crossFade())
+                .centerCrop()
+                .into(background)
         }
     }
 
